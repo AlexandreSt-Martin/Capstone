755e0aa477b34f705563641fc826b0af0565ebe6
dimonoid <dimonoid123@gmail.com>
2022-12-01 13:27:13 -0500

    bug

--- currentLocation.py
+++ currentLocation.py
@@ -1,6 +1,6 @@
 from time import sleep
+
 from geopy.geocoders import Nominatim
-from random import randrange
 
 geoLoc = Nominatim(user_agent="GetLoc")
 
@@ -8,13 +8,13 @@ geoLoc = Nominatim(user_agent="GetLoc")
 def getLocation():
     latitude = 45.419510
     longitude = -75.678770
+    locname = None
     for i in range(10):
         result = str(latitude) + ", " + str(longitude)
         locname = geoLoc.reverse(result)
         print(locname.address)
         latitude += 1
         longitude += 1
-        i = i - 1
     sleep(2)
     return locname
 
